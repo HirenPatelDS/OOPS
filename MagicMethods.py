73c65a14184c1297a83fb21efeec25723b1ae9bd
Hiren Patel <hirenpatel.ds@gmail.com>
2025-07-21 12:21:16 +0530

    Added MagicMethods

---
+++ MagicMethods.py
@@ -0,0 +1,9 @@
+# Magic Methods/Dunder Methods
+#  Magic methods are special methods and they have super power (__name__) 
+# __init__(Constructor) is also Magic Powers - You don't have to call it
+
+class Temp:
+    def __init__(self):
+        print("Hello")
+
+obj = Temp()
